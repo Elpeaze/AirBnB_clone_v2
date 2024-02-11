0b483b4737efdab1a08a5c2eeac6767d1a2b7d64
Elpeaze <elpeaze@gmail.com>
2024-02-11 08:40:49 +0100

    Task 2 done!

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,15 @@ from datetime import datetime
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
+        """Instantiates a new model"""
+        if (not kwargs or 'id' not in kwargs):
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            if kwargs:
+                for k, v in kwargs.items():
+                    setattr(self, k, v)
             storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
